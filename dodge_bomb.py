fdec12bf4d5ad8a4c10c29b9c9d9afec2aaa5040
mochi <c0b2214502@edu.teu.ac.jp>
2023-10-03 17:26:00 +0900

    追加機能1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -34,9 +34,14 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img_game_over = pg.image.load("ex02/fig/0.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_game_over = pg.transform.rotozoom(kk_img_game_over, 0, 2.0)
+
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)  #練習３：こうかとんの初期座標を設定する
+    game_over = False #追加機能3：ゲームオーバーのフラグ 
+
     """ばくだん"""
     bd_img = pg.Surface((20, 20)) #練習１：爆弾Surfaceを作成する
     bd_img.set_colorkey((0, 0, 0)) #練習１：黒い部分を透明にする
@@ -48,15 +53,48 @@ def main():
 
     clock = pg.time.Clock()
     tmr = 0
+    game_over = False
+    game_over_time = 3000
+    bom_imgs = []
+    for r in range(1, 11):
+        bom_img = pg.Surface((20 * r, 20 * r))
+        pg.draw.circle(bom_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+        bom_imgs.append(bom_img)
+        bom_img.set_colorkey((0, 0, 0))
+        bom_img = bom_imgs[min(tmr // 500, 9)]
+        key_lis ={(-5, 0): pg.transform.rotozoom(kk_img, 0, 1.0), 
+                  (-5, -5): pg.transform.rotozoom(kk_img, -45, 1.0),
+                  (0, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, -90, 1.0), True, False),
+                  (+5, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, -45, 1.0), True, False),
+                  (+5, 0): pg.transform.flip(kk_img, True, False),
+                  (+5, +5): pg.transform.flip(pg.transform.rotozoom(kk_img, 45, 1.0), True, False),
+                  (0, +5): pg.transform.flip(pg.transform.rotozoom(kk_img, 90, 1.0), True, False),
+                  (-5, +5): pg.transform.rotozoom(kk_img, 45, 1.0),}
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         
-        if kk_rct.colliderect(bd_rct): #練習５：ぶつかってたら
+        if not game_over and kk_rct.colliderect(bd_rct): #練習５：ぶつかってたら
             print("ゲームオーバー")
             return
         
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+
+        for k, mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+            for l, m in key_lis.items():
+                if sum_mv[0] == l[0] and sum_mv[1] ==l[1]:
+                    kk_img = m  
+
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) 
+
         screen.blit(bg_img, [0, 0])
         
         """こうかとん"""
@@ -79,6 +117,11 @@ def main():
             vy *= -1
         screen.blit(bd_img, bd_rct) #練習１：Rectを使って試しにblit
         
+        if game_over:
+            screen.blit(kk_img2, kk_rct)
+        else:
+            screen.blit(kk_img, kk_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
